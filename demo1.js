22893dec501ecaa29e7d97327c72e49abaa536b9
Neil Zhang <jerry753@foxmail.com>
2020-08-13 14:25:13 +0800

    Add files via upload

---
+++ demo1.js
@@ -0,0 +1,51 @@
+/**
+ * 场景：
+ * 戏剧演出团，演员要去各种场合表演戏剧。
+ * 戏剧分为悲剧（tragedy）和喜剧（comedy）
+ * 剧团会根据观众（audience）人数和剧目类型收费，
+ * 同时还会根据到场观众的数量给出“观众量积分”（volume credit）优惠，积分可以抵折扣。
+ * 
+ * @param {*} invoice 账单
+ * @param {*} plays 演员
+ */
+function statement (invoice, plays) {
+    let totalAmount = 0;
+    let volumeCredits = 0;
+    let result = `Statement for ${invoice.customer}\n`;
+    const format = new Intl.NumberFormat("en-US", {style: "currency", currency: "USD", minimumFractionDigits: 2}).format;
+
+    for (let perf of invoice.performances) {
+        const play = plays[perf.playID];
+        let thisAmount = 0;
+
+        switch (play.type) {
+            case "tragedy":
+                thisAmount = 40000;
+                if (perf.audience > 30) {
+                    thisAmount += 10000 * (perf.audience - 30);
+                }
+                break;
+            case "comedy":
+                thisAmount = 30000;
+                if (perf.audience > 20) {
+                    thisAmount += 10000 + 500 * (perf.audience -20);
+                }
+                thisAmount += 300 * perf.audience;
+                break;
+            default:
+                throw new Error('unknown types: ${play.type}');                  
+        }
+
+        // add volume credits
+        volumeCredits += Math.max(perf.audience - 30, 0);
+        // add extra credit for every ten comedy attendees
+        if ("comedy" === play.type) volumeCredits += Math.floor(perf.audience / 5);
+
+        // print line for this order 
+        result += ` ${play.name}: ${format(thisAmount/100)} (${perf.audience} seats)\n`;
+        totalAmount += thisAmount;
+    }
+    result += `Amount owed is ${format(totalAmount/100)}\n`;
+    result += `You earned ${volumeCredits} credits\n`;
+    return result;
+}
