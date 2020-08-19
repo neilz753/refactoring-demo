780458ec4118d034902c8490ce4c0896da398f25
neil zhang <zhangnan3777@gmail.com>
2020-08-19 21:46:39 +0800

    新增HTML版本和测试案例

--- js/src/index.js
+++ js/src/index.js
@@ -14,6 +14,24 @@ module.exports.statement = function (invoice, plays) {
     return renderPlainText(createStatementData(invoice, plays));
 }
 
+module.exports.htmlStatement = function (invoice, plays) {
+    return renderHtml(createStatementData(invoice, plays));
+}
+
+function renderHtml(data) {
+    let result = `<h1>Statement for ${data.customer}</h1>\n`;
+    result += "<table>\n";
+    result += "<tr><th>play</th><th>seats</th><th>cost</th></tr>\n"
+    for (let perf of data.performances) {
+        result += ` <tr><td>${perf.play.name}</td><td>${perf.audience}</td>`;
+        result += `<td>${ucd(perf.amount)}</td></tr>\n`;
+    }
+    result += "</table>\n";
+    result += `<p>Amount owed is <em>${ucd(data.totalAmount)}</em></p>\n`;
+    result += `<p>You earned <em>${data.totalVolumeCredits}</em> credits</p>\n`;
+    return result;
+}
+
 function renderPlainText(data) {
     let result = `Statement for ${data.customer}\n`;   
     for (let perf of data.performances) {
@@ -23,15 +41,15 @@ function renderPlainText(data) {
     result += `Amount owned is ${ucd(data.totalAmount)}\n`;
     result += `You earned ${data.totalVolumeCredits} credits\n`;
     return result;
+}
 
-    /**
-    * 格式化数字格式 美元
-    * @param {*} aNumber 美分数字
-    */
-    function ucd(aNumber) {
-        return new Intl.NumberFormat("en-US", {
+/**
+* 格式化数字格式 美元
+* @param {*} aNumber 美分数字
+*/
+function ucd(aNumber) {
+    return new Intl.NumberFormat("en-US", {
         style: "currency", 
         currency: "USD", 
         minimumFractionDigits: 2}).format(aNumber/100);
-    }
 }
