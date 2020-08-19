b8477d93138df20a2fde9349a091a36fe376b86f
neil zhang <zhangnan3777@gmail.com>
2020-08-19 21:03:38 +0800

    拆分两个文件，但测试案例也要改todo

--- js/src/index.js
+++ js/src/index.js
@@ -1,3 +1,4 @@
+import createStatementData from './createStatementData.js';
 /**
  * 
  * 场景：
@@ -10,83 +11,10 @@
  * @param {*} plays 演员
  */
 function statement (invoice, plays) {
-    const statementData = {};
-    statementData.customer = invoice.customer;
-    statementData.performances = invoice.performances.map(enrichPerformance);
-    statementData.totalAmount = totalAmount(statementData);
-    statementData.totalVolumeCredits = totalVolumeCredits(statementData);
-    return renderPlainText(statementData, plays);
-  
-    function enrichPerformance(aPerformance) {
-        // 返回一个浅副本
-        const result = Object.assign({}, aPerformance);
-        result.play = playFor(result);
-        result.amount = amountFor(result);
-        result.volumeCredits = volumeCreditsFor(result);
-        return result;
-    }
-
-    /**
-     * 获取当前剧的playID
-     * @param {*} aPerformance 剧
-     */
-    function playFor(aPerformance) {
-        return plays[aPerformance.playID];
-    }
-
-    /**
-     * 计算当前剧的演出费用
-     * @param {*} aPerformance 剧
-     */
-    function amountFor(aPerformance) {
-        let result = 0;
-        switch (aPerformance.play.type) {
-        case "tragedy":
-            result = 40000;
-            if (aPerformance.audience > 30) {
-                result += 1000 * (aPerformance.audience - 30);
-            }
-            break;
-        case "comedy":
-            result = 30000;
-            if (aPerformance.audience > 20) {
-                result += 10000 + 500 * (aPerformance.audience - 20);
-            }
-            result += 300 * aPerformance.audience;
-            break;
-        default:
-            throw new Error(`unknown type: ${aPerformance.play.type}`);
-        }
-        return result;
-    }
-
-    /**
-     * 计算当前剧的观众量积分
-     * @param {*} aPerformance 剧
-     */
-    function volumeCreditsFor(aPerformance) {
-        let result = 0;
-        result += Math.max(aPerformance.audience - 30, 0);
-        if ("comedy" === aPerformance.play.type) result += Math.floor(aPerformance.audience / 5);
-        return result;
-    }
-
-    /**
-    * 总金额
-    */
-    function totalAmount(data) {
-        return data.performances.reduce((total, p) => total + p.amount, 0);
-    }
-
-    /**
-    * 总观众量积分
-    */
-    function totalVolumeCredits(data) {
-        return data.performances.reduce((total, p) => total + p.volumeCredits, 0);
-    }
+    return renderPlainText(createStatementData(invoice, plays));
 }
 
-function renderPlainText(data, plays) {
+function renderPlainText(data) {
     let result = `Statement for ${data.customer}\n`;   
     for (let perf of data.performances) {
         // print line for this order
