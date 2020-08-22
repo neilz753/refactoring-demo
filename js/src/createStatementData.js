7e49aa3263c513c8d08a774d275ea0869c96ddd4
neil zhang <zhangnan3777@gmail.com>
2020-08-22 17:50:42 +0800

    支持更多类型的戏剧计算，类型多态，以多态取代表达式条件

--- js/src/createStatementData.js
+++ js/src/createStatementData.js
@@ -7,11 +7,12 @@ module.exports = function (invoice, plays) {
     return result;
 
     function enrichPerformance(aPerformance) {
+        const calculator = cretePerformanceCalculator(aPerformance, playFor(aPerformance));
         // 返回一个浅副本
         const result = Object.assign({}, aPerformance);
-        result.play = playFor(result);
-        result.amount = amountFor(result);
-        result.volumeCredits = volumeCreditsFor(result);
+        result.play = calculator.play;
+        result.amount = calculator.amount;
+        result.volumeCredits = calculator.volumeCredits;
         return result;
     }
 
@@ -21,43 +22,6 @@ module.exports = function (invoice, plays) {
      */
     function playFor(aPerformance) {
         return plays[aPerformance.playID];
-    }
-
-    /**
-     * 计算当前剧的演出费用
-     * @param {*} aPerformance 剧
-     */
-    function amountFor(aPerformance) {
-        let result = 0;
-        switch (aPerformance.play.type) {
-        case "tragedy":
-            result = 40000;
-            if (aPerformance.audience > 30) {
-                result += 1000 * (aPerformance.audience - 30);
-            }
-            break;
-        case "comedy":
-            result = 30000;
-            if (aPerformance.audience > 20) {
-                result += 10000 + 500 * (aPerformance.audience - 20);
-            }
-            result += 300 * aPerformance.audience;
-            break;
-        default:
-            throw new Error(`unknown type: ${aPerformance.play.type}`);
-        }
-        return result;
-    }
-
-    /**
-     * 计算当前剧的观众量积分
-     * @param {*} aPerformance 剧
-     */
-    function volumeCreditsFor(aPerformance) {
-        let result = 0;
-        result += Math.max(aPerformance.audience - 30, 0);
-        if ("comedy" === aPerformance.play.type) result += Math.floor(aPerformance.audience / 5);
-        return result;
     }
 
     /**
@@ -74,4 +38,59 @@ module.exports = function (invoice, plays) {
         return data.performances.reduce((total, p) => total + p.volumeCredits, 0);
     }
     
+}
+
+function cretePerformanceCalculator(aPerformance, aPlay) {
+    switch(aPlay.type) {
+    case "tragedy": return new TragedyCalculator(aPerformance, aPlay);
+    case "comedy": return new ComedyCalculator(aPerformance, aPlay);
+    default:
+        throw new Error(`unknown type: ${aPlay.type}`);
+    }
+}
+
+class PerformanceCalculator {
+    constructor(aPerformance, aPlay) {
+        this.performance = aPerformance;
+        this.play = aPlay;
+    }
+
+    /**
+     * 计算当前剧的演出费用
+     */
+    get amount() {
+        throw new Error(`subclass responsibility`);
+    }
+
+    /**
+     * 计算当前剧的观众量积分
+     */
+    get volumeCredits() {
+        return Math.max(this.performance.audience - 30, 0);
+    }
+}
+
+class TragedyCalculator extends PerformanceCalculator {
+    get amount() {
+        let result = 40000;
+        if (this.performance.audience > 30) {
+            result += 1000 * (this.performance.audience - 30);
+        }
+        return result;
+    }
+}
+
+class ComedyCalculator extends PerformanceCalculator {
+    get amount() {
+        let result = 30000;
+        if (this.performance.audience > 20) {
+            result += 10000 + 500 * (this.performance.audience - 20);
+        }
+        result += 300 * this.performance.audience;
+        return result;
+    }
+
+    get volumeCredits() {
+        return super.volumeCredits + Math.floor(this.performance.audience / 5);
+    }
 }
